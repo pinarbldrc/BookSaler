b3c8722e470bd52800fc608d8f175d58594c9ead
pinar-mat <63678861+pinar-mat@users.noreply.github.com>
2020-10-13 21:05:07 +0300

    Kitap kiralama otomasyonunun ilk yüklemesidir

---
+++ src/main/java/com/pinar/BookSaler/BookSalerApplication.java
@@ -0,0 +1,13 @@
+package com.pinar.BookSaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSalerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinar/BookSaler/BookSalerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinar.BookSaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSalerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
